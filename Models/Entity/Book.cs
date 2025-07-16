44e22ab6521a27aa151ae2b19375a69393c26d72
Zhiyun Qin <96156298+Okita1027@users.noreply.github.com>
2025-07-16 17:31:19 +0800

    init

---
+++ Models/Entity/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace DEMO_CRUD.Models.Entity
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get => title; set => title = value; }
+
+        private string title;
+
+        public Book(string title)
+        {
+            this.title = title;
+        }
+    }
+}
