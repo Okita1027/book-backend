a8cad9de741222ceaeb9dad1e6150adab2a76799
Zhiyun Qin <96156298+Okita1027@users.noreply.github.com>
2025-07-17 17:11:40 +0800

    出版社基础CRUD

---
+++ Controllers/PublishersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DEMO_CRUD.Data;
+using DEMO_CRUD.Models.Entity;
+using DEMO_CRUD.Models.DTO;
+
+namespace DEMO_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PublishersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Publishers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Publisher>>> GetPublishers()
+        {
+            return await _context.Publishers.ToListAsync();
+        }
+
+        // GET: api/Publishers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Publisher>> GetPublisher(int id)
+        {
+            var publisher = await _context.Publishers.FindAsync(id);
+
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return publisher;
+        }
+
+        // PUT: api/Publishers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPublisher(int id, EditPublisherDTO editPublisherDTO)
+        {
+            // 1.判断请求数据是否有效【符合注解的要求】
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            // 2.查找现有实体
+            var existingPublisher = await _context.Publishers.FindAsync(id);
+            if (existingPublisher == null)
+            {
+                return NotFound("此出版社不存在！");
+            }
+            // 3.更新现有实体的属性
+            existingPublisher.Name = editPublisherDTO.Name;
+            // 4.尝试保存更改
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PublisherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Publishers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Publisher>> PostPublisher(EditPublisherDTO editPublisherDTO)
+        {
+            var publisher = new Publisher
+            {
+                Name = editPublisherDTO.Name
+            };
+            _context.Publishers.Add(publisher);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPublisher", new { id = publisher.Id }, publisher);
+        }
+
+        // DELETE: api/Publishers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePublisher(int id)
+        {
+            var publisher = await _context.Publishers.FindAsync(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            _context.Publishers.Remove(publisher);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PublisherExists(int id)
+        {
+            return _context.Publishers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/DTO/EditPublisherDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DEMO_CRUD.Models.DTO
+{
+    public class EditPublisherDTO
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } // 出版社名称
+    }
+}
