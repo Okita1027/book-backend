652264b2599e0b0be35079297e6ea21e6f08dc81
Zhiyun Qin <96156298+Okita1027@users.noreply.github.com>
2025-08-15 08:02:37 +0800

    重构，但是BUG待解决： 无限弹窗：发生未预期的错误

---
+++ BookFrontend/Controls/PasswordField.cs
@@ -0,0 +1,38 @@
+using System.Windows;
+using System.Windows.Controls;
+using MaterialDesignThemes.Wpf;
+
+namespace book_frontend.Controls
+{
+    public class PasswordField : Control
+    {
+        static PasswordField()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PasswordField), new FrameworkPropertyMetadata(typeof(PasswordField)));
+        }
+
+        public static readonly DependencyProperty HintTextProperty =
+            DependencyProperty.Register(nameof(HintText), typeof(string), typeof(PasswordField), new PropertyMetadata(string.Empty));
+        public string HintText
+        {
+            get => (string)GetValue(HintTextProperty);
+            set => SetValue(HintTextProperty, value);
+        }
+
+        public static readonly DependencyProperty IconKindProperty =
+            DependencyProperty.Register(nameof(IconKind), typeof(PackIconKind), typeof(PasswordField), new PropertyMetadata(PackIconKind.Lock));
+        public PackIconKind IconKind
+        {
+            get => (PackIconKind)GetValue(IconKindProperty);
+            set => SetValue(IconKindProperty, value);
+        }
+
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register(nameof(Password), typeof(string), typeof(PasswordField), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        public string Password
+        {
+            get => (string)GetValue(PasswordProperty);
+            set => SetValue(PasswordProperty, value);
+        }
+    }
+}
